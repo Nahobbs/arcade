374ccaa6378b54c8247986a191443a9605c77be0
Nick Hobbs <nah13734@uga.edu>
2019-11-20 11:54:34 -0500

    made the pits buttons and styled them

--- src/main/java/cs1302/mancala/Mancala.java
+++ src/main/java/cs1302/mancala/Mancala.java
@@ -20,87 +20,71 @@ import javafx.scene.layout.VBox;
 
 public class Mancala extends Application {
 
+    //initial ints that represent the amount of stones in a pit or store
     //the pits of the game
-    private int p1 = 4;
-    private int p2 = 4;
-    private int p3 = 4;
-    private int p4 = 4;
-    private int p5 = 4;
-    private int p6 = 4;
-    private int p7 = 4;
-    private int p8 = 4;
-    private int p9 = 4;
-    private int p10 = 4;
-    private int p11 = 4;
-    private int p12 = 4;
+    private static int p1 = 4;
+    private static int p2 = 4;
+    private static int p3 = 4;
+    private static int p4 = 4;
+    private static int p5 = 4;
+    private static int p6 = 4;
+    private static int p7 = 4;
+    private static int p8 = 4;
+    private static int p9 = 4;
+    private static int p10 = 4;
+    private static int p11 = 4;
+    private static int p12 = 4;
     //player stores for their collected pieces(points)
-    private int store1 = 0;
-    private int store2 = 0;
+    private static int store1 = 0;
+    private static int store2 = 0;
+
+    //shapes in the scene graph that are the pits and stores
+    //player 1's pits
+    // private static Circle pit1 = new Circle(20, 20, 70);
+    // private static Circle pit2 = new Circle(20, 20, 70);
+    // private static Circle pit3 = new Circle(20, 20, 70);
+    // private static Circle pit4 = new Circle(20, 20, 70);
+    // private static Circle pit5 = new Circle(20, 20,70);
+    // private static Circle pit6 = new Circle(20, 20, 70);
+    // //player 2's pits
+    // private static Circle pit7 = new Circle(20, 20, 70);
+    // private static Circle pit8 = new Circle(20, 20, 70);
+    // private static Circle pit9 = new Circle(20, 20, 70);
+    // private static Circle pit10 = new Circle(20, 20, 70);
+    // private static Circle pit11 = new Circle(20, 20, 70);
+    // private static Circle pit12 = new Circle(20, 20, 70);
+    //both players stores
+    private static Rectangle p1Store = new Rectangle(110.0, 300.0);
+    private static Rectangle p2Store = new Rectangle(110.0, 300.0);
+
+    private static GridPane board = new GridPane();
+
+    private static Button pit1 = new Button("" + p1);
+    private static Button pit2 = new Button("" + p2);
+    private static Button pit3 = new Button("" + p3);
+    private static Button pit4 = new Button("" + p4);
+    private static Button pit5 = new Button("" + p5);
+    private static Button pit6 = new Button("" + p6);
+    private static Button pit7 = new Button("" + p7);
+    private static Button pit8 = new Button("" + p8);
+    private static Button pit9 = new Button("" + p9);
+    private static Button pit10 = new Button("" + p10);
+    private static Button pit11 = new Button("" + p11);
+    private static Button pit12 = new Button("" + p12);
+
 
     @Override
     public void start(Stage stage) {
-        GridPane board = new GridPane();
         board.setPadding(new Insets(10, 10, 10, 10));
         board.setHgap(6);
         board.setVgap(6);
 
-        //player 1's pits
-        Circle pit1 = new Circle(20, 20, 70);
-        Circle pit2 = new Circle(20, 20, 70);
-        Circle pit3 = new Circle(20, 20, 70);
-        Circle pit4 = new Circle(20, 20, 70);
-        Circle pit5 = new Circle(20, 20,70);
-        Circle pit6 = new Circle(20, 20, 70);
-        //player 2's pits
-        Circle pit7 = new Circle(20, 20, 70);
-        Circle pit8 = new Circle(20, 20, 70);
-        Circle pit9 = new Circle(20, 20, 70);
-        Circle pit10 = new Circle(20, 20, 70);
-        Circle pit11 = new Circle(20, 20, 70);
-        Circle pit12 = new Circle(20, 20, 70);
-
-        //both players stores
-        Rectangle p1Store = new Rectangle(110.0, 300.0);
-        p1Store.setArcWidth(110.0);
-        p1Store.setArcHeight(110.0);
-        Rectangle p2Store = new Rectangle(110.0, 300.0);
-        p2Store.setArcWidth(110.0);
-        p2Store.setArcHeight(110.0);
-
-        p1Store.setId("rect");
-        p2Store.setId("rect");
-        pit1.setId("circ");
-        pit2.setId("circ");
-        pit3.setId("circ");
-        pit4.setId("circ");
-        pit5.setId("circ");
-        pit6.setId("circ");
-        pit7.setId("circ");
-        pit8.setId("circ");
-        pit9.setId("circ");
-        pit10.setId("circ");
-        pit11.setId("circ");
-        pit12.setId("circ");
+        setIds();
+        addShapes();
 
         //adds rudimentary objects to the gridpane
 //        board.add(p1Store, 0, 0);
 //        board.add(p2Store, 0, 7);
-
-        //adds p1 pits
-        board.add(pit1, 0, 0);
-        board.add(pit2, 1, 0);
-        board.add(pit3, 2, 0);
-        board.add(pit4, 3, 0);
-        board.add(pit5, 4, 0);
-        board.add(pit6, 5, 0);
-        //adds p2 pits
-        board.add(pit7, 0, 1);
-        board.add(pit8, 1, 1);
-        board.add(pit9, 2, 1);
-        board.add(pit10, 3, 1);
-        board.add(pit11, 4, 1);
-        board.add(pit12, 5, 1);
-
 
         HBox Hbox = new HBox();
 
@@ -132,5 +116,28 @@ public class Mancala extends Application {
             System.exit(1);
         } // try
     } // main
+
+    public static void setIds() {
+        p1Store.setId("rect");
+        p2Store.setId("rect");
+    } //setIds
+
+    public static void addShapes() {
+        //adds p1 pits
+        board.add(pit1, 0, 0);
+        board.add(pit2, 1, 0);
+        board.add(pit3, 2, 0);
+        board.add(pit4, 3, 0);
+        board.add(pit5, 4, 0);
+        board.add(pit6, 5, 0);
+        //adds p2 pits
+        board.add(pit7, 0, 1);
+        board.add(pit8, 1, 1);
+        board.add(pit9, 2, 1);
+        board.add(pit10, 3, 1);
+        board.add(pit11, 4, 1);
+        board.add(pit12, 5, 1);
+    } //addShapes
+
 
 } //Mancala
