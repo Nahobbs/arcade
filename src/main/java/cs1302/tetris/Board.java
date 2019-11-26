7c1dbfe7102fcc88de2f3b5d17c021070c9265ea
Ethan Simms <ecs18466@uga.edu>
2019-11-26 10:12:40 -0500

    added more functionality to Board

--- src/main/java/cs1302/tetris/Board.java
+++ src/main/java/cs1302/tetris/Board.java
@@ -2,7 +2,7 @@ package cs1302.tetris;
 
 import cs1302.tetris.Shape.Tetrominoe;
 import javafx.animation.Timeline;
-import javafx.scene.Control.*;
+//import javafx.scene.Control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
@@ -21,13 +21,34 @@ public class Board {
     private VBox board;
     private VBox status;
     private HBox container;
+    private Tetrominoe[] shapes;
+    private Shape dropPiece;
+    private Shape nextPiece;
 
     /**
      * The default constructor for the Board.
      */
     public Board() {
+        fallen = false;
+        //anime = new Timeline();
+        board = new VBox();
+        status = new VBox();
+        container = new HBox();
+        shapes = new Tetrominoe[WIDTH * HEIGHT];
 
     } //Board constructor
+
+    /**
+     * Returns the shape currently located at the index on the board.
+     * @param x the x location of the piece
+     * @param y the y location of the piece
+     * @return the joint location in the array given the params
+     */
+    public Tetrominoe shape(int x, int y) {
+        return shapes[y * WIDTH + x];
+    } //shape
+
+
 
 
 } //Board
